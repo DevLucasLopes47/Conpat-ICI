5d804263e1b8b4bf81ae4858ccda2404cd665b74
Lucas Gabriel Lopes de Souza <lucaslpsouza2022@gmail.com>
2024-12-31 15:08:47 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,16 +6,13 @@ const path = require('path');
 
 const app = express();
 
-// Rotas do projeto
 const patrimoniosRoutes = require('./routes/patrimoniosRoutes');
 const authRoutes = require('./routes/authRoutes');
 
-// Middleware para JSON e formulários
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-// Configuração de sessões
 app.use(
     session({
         secret: process.env.SESSION_SECRET || 'fallback-secret-key',
@@ -24,39 +21,22 @@ app.use(
     })
 );
 
-// Middleware de autenticação
 app.use((req, res, next) => {
-    const rotasPublicas = [
-        '/auth/login',
-        '/html/tela.html',
-        '/auth/logout',
-        '/css/',
-        '/images/',
-        '/js/',
-    ];
+    const rotasPublicas = ['/auth/login', '/html/tela.html', '/auth/logout', '/css/', '/images/', '/js/'];
     const isRotaPublica = rotasPublicas.some((rota) => req.path.startsWith(rota));
 
     if (isRotaPublica || req.session.usuarioAutenticado) {
         next();
     } else {
-        res.redirect('/html/tela.html'); // Redireciona para a tela de login
+        res.redirect('/html/tela.html'); 
     }
 });
 
-// Rotas de autenticação
 app.use('/auth', authRoutes);
 
-// Rotas de patrimônio
 app.use('/api/patrimonios', patrimoniosRoutes);
 
-// Configuração para servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Rota principal (raiz) direciona para a tela de login
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/html/tela.html'));
-});
-
-// Inicia o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
